71b96d87f5d4df0e3f9fce2aae4a1021c2d1266e
Camila <camila.rosa@vtex.com>
2025-03-19 09:46:14 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Home button
+const btnHome = document.querySelector('#button-home');
+btnHome.addEventListener("click", function() {
+    window.location.href = "./pages/createListOptions.html";
+
+});
+
+// "./pages/createListOptions.html";

b7e9a89f3723b0a5a27f2ca40ebb23fa83f9d6dc
Camila <camila.rosa@vtex.com>
2025-03-19 18:06:40 -0300

    Options page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Header
+const headerHome = document.querySelector('#header')
+headerHome.innerHTML = `
+    <a href="../index.html"><img id="logo" src="./assets/logo/logo.png" alt="logo da Yotei"></a>
+`
+const headerPages = document.querySelector('#header')
+headerPages.innerHTML = `
+    <a href="../index.html"><img id="logo" src="../assets/logo/logo.png" alt="logo da Yotei"></a>
+`
+
+//Footer
+const footer = document.querySelector('#footer')
+footer.innerHTML = `
+    <h1 class="app-name">Yotei</h1>
+    <p>Desenvolvido por Camila da Rosa</p>
+    <div id="social-links">
+        <a href="https://www.linkedin.com/in/caamiladarosa/" target="_blank" alt="Ícone do linkedin"><ion-icon name="logo-linkedin"></ion-icon></a>
+        <a href="mailto:caamiladarosa@gmail.com" target="_blank" alt="Ícone do e-mail"><ion-icon name="mail"></ion-icon></a>
+    </div>
+`
+
 // Home button
 const btnHome = document.querySelector('#button-home');
 btnHome.addEventListener("click", function() {
@@ -5,4 +26,8 @@ btnHome.addEventListener("click", function() {
 
 });
 
-// "./pages/createListOptions.html";
+// Create list without saving
+const btnCreateList = document.querySelector('#create-list');
+btnCreateList.addEventListener("click", function() {
+    window.location.href = "https://caamiladarosa.github.io/yotei/pages/createList.html";
+});
